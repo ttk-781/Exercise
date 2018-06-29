4214f6559294730ca02c52f0f5e5c9920fd0b7a2
ttk-781 <31058811+ttk-781@users.noreply.github.com>
2018-06-29 16:44:02 +0530

    4.43

--- YouTube Video 1/src/day1/ExampleFor.java
+++ YouTube Video 1/src/day1/ExampleFor.java
@@ -5,7 +5,7 @@ public class ExampleFor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int i = 7; i < 10; i++) {
+		for (int i = 1; i < 10; i++) {
 		System.out.println(i);
 		}
 	}
