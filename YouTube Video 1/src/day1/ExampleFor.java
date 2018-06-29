87bb64d24ecdae39aa2ab6d20682b840717c70cb
ttk-781 <31058811+ttk-781@users.noreply.github.com>
2018-06-29 17:05:50 +0530

    Update ExampleFor.java

--- YouTube Video 1/src/day1/ExampleFor.java
+++ YouTube Video 1/src/day1/ExampleFor.java
@@ -5,7 +5,7 @@ public class ExampleFor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		for (int i = 1; i < 10; i++) {
+		for (int 5 = 1; i < 10; i++) {
 		System.out.println(i);
 		}
 	}
